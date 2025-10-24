ad115f568ee1405e2a4cd8d9fa33eec9e963da07
cobaltdonkey <63113363+cobaltdonkey@users.noreply.github.com>
2025-10-24 15:43:51 +0000

    I tried

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,9 +3,21 @@
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                newItem: ""
             }
             
+        },
+        methods: {
+            addItem() {
+                if (this.newItem.trim() != "") {
+                    this.items.push(this.newItem.trim());
+                    this.newItem = "";
+                }
+            },
+            removeItem(idx) {
+                this.items.splice(idx,1);
+            }
         }
     }
 </script>
@@ -14,5 +26,12 @@
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
-   
+     <ul>
+        <li v-for="(item, idx) in items" :key="idx">
+            {{ item }}
+            <button @click="removeItem(idx)">Delete!</button>
+        </li>
+     </ul>
+     <input type="text" v-model="newItem">
+     <button @click="addItem">Add!</button>
 </template>
--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -1,9 +1,49 @@
 <script>
+import axios from 'axios';
+
     export default { 
 
        // add code here
-
+       data() {
+            return {
+                subject: "",
+                entry: "",
+                moods: ['Happy', 'Sad', 'Angry'],
+                selectedMood: "",
+                outputMsg: ""
+            }
+       },
+       computed:{
+        baseUrl() {
+            if (window.location.hostname == 'localhost') {
+                return 'https://localhost:3000/is216/blog'
+            } else {
+                const codespace_host = window.location.hostname.replace('5173', '3000')
+                return 'https://localhost/is216/blog';
+            }
+        }
+       },
+       methods: {
+            addPost() {
+                axios.get(`${this.baseUrl}/addPost`, {
+                    params: {
+                        subject: this.subject,
+                        entry: this.entry,
+                        mood: this.selectedMood
+                    }
+                })
+                .then(response => {
+                    this.outputMsg = response.data.message;
+                    this.subject = "";
+                    this.entry = "".
+                    this.selectedMood = "";
+                })
+                .catch(error => {
+                    console.error('Error adding post:', error);
+                })
+            }
     }
+}
 </script>
 
 <template>
@@ -19,12 +59,17 @@
 
         Mood:
         <!-- TODO: Build a dropdown list here for selecting the mood -->
-
+        <select v-model="selectedMood">
+            <option v-for="mood in moods">
+                {{ mood }}
+            </option>
+        </select>
         <br>
 
         <br>
-        <button>Submit New Post</button>
-
+        <p v-if="outputMsg">{{ outputMsg }}</p>
+        <button @click="addPost">Submit New Post</button>
+        
         <hr> Click  <a><router-link to="/ViewPosts/">here</router-link></a>  to return to Main Page
        
     </div>
